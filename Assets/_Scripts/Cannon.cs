fbd22785c0f2e1e5ade2113987fcb889ad138f4f
Miguel Sanchez <mangelsr25@gmail.com>
2025-08-31 00:41:51 -0500

    [ADD] ShaderGraph example with material appied to Ball, [ADD] Particle effect used on Cannon shoot method

--- Assets/_Scripts/Cannon.cs
+++ Assets/_Scripts/Cannon.cs
@@ -5,6 +5,8 @@ public class Cannon : MonoBehaviour
 
     public static bool isBlocked;
     [SerializeField] GameObject ballPrefab;
+    [SerializeField] GameObject particlesPrefab;
+
     GameObject cannonTip;
     float rotation;
 
@@ -27,11 +29,15 @@ public class Cannon : MonoBehaviour
         if (!isBlocked && GameManager.ShootsPerGame > 0 && Input.GetKeyDown(KeyCode.Space))
         {
             GameObject temp = Instantiate(ballPrefab, cannonTip.transform.position, transform.rotation);
-            CameraFollow.objective = temp; 
+            CameraFollow.objective = temp;
             Rigidbody tempRB = temp.GetComponent<Rigidbody>();
             Vector3 shootDirection = transform.rotation.eulerAngles;
             shootDirection.y = 90 - shootDirection.x;
             tempRB.velocity = shootDirection.normalized * GameManager.BallSpeed;
+            Vector3 particlesDirection = new Vector3(-90 + shootDirection.x, 90, 9);
+            Instantiate(
+                particlesPrefab, cannonTip.transform.position, Quaternion.Euler(particlesDirection), transform
+            );
             GameManager.ShootsPerGame--;
             isBlocked = true;
         }
